bbe3e99aa173a235e0cb3e859a10c68ab4806800
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-04-08 19:56:07 -0500

    Class Part One studio

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+
+    private ArrayList<MenuItems> menuItems = new ArrayList<>();
+    public Date dateUpdated;
+}
---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/MenuItems.java
@@ -0,0 +1,59 @@
+package org.launchcode;
+
+public class MenuItems {
+
+    public String name;
+    public Float price;
+    public String description;
+    public String category;
+    public boolean isNew;
+
+
+    public MenuItems(String name, Float price, String description, String category, boolean isNew) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.category = category;
+        this.isNew = isNew;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+}
