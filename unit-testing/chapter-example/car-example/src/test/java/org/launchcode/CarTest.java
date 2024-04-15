6a5e4ecf0a26deeb32f19629790de774896b1361
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-04-15 12:19:43 -0500

    Unit Testing exercises

--- unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
+++ unit-testing/chapter-example/car-example/src/test/java/org/launchcode/CarTest.java
@@ -10,14 +10,15 @@ class CarTest {
 
     @BeforeEach
     public void createCarObject() {
-        test_car = new Car("Toyota", "Prius",10,50);
+        test_car = new Car("Toyota", "Prius", 10, 50);
     }
 
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitLab account)
     @Test
     public void emptyTest() {
-        assertEquals(10,10,.001);
+        assertEquals(10, 10, .001);
     }
+
     //TODO: constructor sets gasTankLevel properly
     //TODO: constructor sets gasTankLevel properly
     @Test
@@ -27,4 +28,19 @@ class CarTest {
     //TODO: gasTankLevel is accurate after driving within tank range
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
     //TODO: can't have more gas than tank size, expect an exception
+
+
+    //Compare value to 0
+    @Test
+    public void testInitialGasTankEmpty() {
+        assertFalse(test_car.getGasTankLevel() == 0);
+    }
+
+    //Another version with assertTrue
+    @Test
+    public void testInitialGasTankTrue() {
+        assertTrue(test_car.getGasTankLevel() > 0);
+    }
+
+
 }
