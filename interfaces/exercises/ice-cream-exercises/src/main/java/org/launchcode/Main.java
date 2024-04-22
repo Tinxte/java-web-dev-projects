2498e1afc6af75d30ca344031f400b4d9719f31d
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-04-22 17:21:53 -0500

    Interfaces exercises

--- interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
+++ interfaces/exercises/ice-cream-exercises/src/main/java/org/launchcode/Main.java
@@ -1,6 +1,7 @@
 package org.launchcode;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,8 +11,18 @@ public class Main {
 
         // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
 
+        flavors.sort(new FlavorComparator());
+
         // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
 
+        cones.sort(new ConeComparator());
+
         // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        System.out.println("\nFlavors sorted: \n");
+        System.out.println(flavors);
+
+        System.out.println("\nCones sorted: \n");
+        cones.toString();
+        System.out.println(cones);
     }
 }
